d777cb537907e195b262705cc24a4c3b978a357c
cheneyweb <457299596@qq.com>
2019-01-07 17:09:32 +0800

    初版移动UI

--- member-mobile/src/views/Person.vue
+++ member-mobile/src/views/Person.vue
@@ -1,8 +1,73 @@
 <template>
-  <div></div>
+  <v-layout column>
+    <v-list>
+      <v-subheader>个人资料</v-subheader>
+      <v-list-tile>
+        <v-list-tile-content>姓名：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>编号：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>证件号码：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>居住地：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>手机号码：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>微信号：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>银行账户：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>级别：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>代理区域：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>安置编号：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-content>推荐编号：</v-list-tile-content>
+        <v-list-tile-content class="align-end"></v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <v-footer height="auto" color="lighten-1">
+      <v-layout column justify-center>
+        <v-btn color="error" @click="logout">登出</v-btn>
+        <v-flex xs12 align-self-center>
+          <div>如需修改资料，请联系当地市场工作人员</div>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-layout>
 </template>
+
 <script>
 export default {
-  components: {}
+  data: () => ({
+    items: []
+  }),
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push({ path: "/" });
+    }
+  }
 };
 </script>
--- member-mobile/src/views/Team.vue
+++ member-mobile/src/views/Team.vue
@@ -1,8 +1,48 @@
 <template>
-  <div></div>
+  <v-layout column>
+    <v-flex xs12>
+      <v-list subheader three-line>
+        <v-subheader>团队建设</v-subheader>
+        <template v-for="(item, index) in items">
+          <v-list-tile :key="item.title" avatar ripple @click="diplomacy">
+            <v-list-tile-avatar>
+              <v-icon :class="[item.iconClass]">{{item.icon}}</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+        </template>
+      </v-list>
+    </v-flex>
+  </v-layout>
 </template>
+
 <script>
 export default {
-  components: {}
+  data: () => ({
+    items: [
+      {
+        active: true,
+        title: "会员注册",
+        subtitle: "手动注册新会员",
+        icon: "person_add"
+        // iconClass: "grey lighten-1 white--text"
+      },
+      {
+        active: true,
+        title: "扫码注册",
+        subtitle: "扫码注册新会员",
+        icon: "person_add"
+      }
+    ]
+  }),
+  methods: {
+    diplomacy() {
+      console.log("外交");
+    }
+  }
 };
 </script>
