cd9fd9bee22d20429ce8d4bd30800533659ebf60
cheneyweb <457299596@qq.com>
2019-01-08 11:15:24 +0800

    我的账户页

--- member-mobile/src/components/MyAccount.vue
+++ member-mobile/src/components/MyAccount.vue
@@ -12,13 +12,21 @@
             <v-btn dark flat @click="openMyAccount = false">提现申请</v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <v-list>
-          <!-- <v-subheader>我的账户</v-subheader> -->
-          <v-list-tile>
-            <v-list-tile-content>账户余额：</v-list-tile-content>
-            <v-list-tile-content class="align-end">1000.00</v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+        <v-container>
+          <v-layout wrap>
+            <v-flex xs12>
+              <v-text-field box prefix="￥" label="账户余额" readonly required value="1000.00"></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field
+                label="提现金额"
+                hint="本月免费提现次数：2，提现手续费10元/次"
+                :rules="[rules.price]"
+                clearable
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-card>
     </v-dialog>
   </v-layout>
@@ -26,7 +34,14 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      rules: {
+        price: value => {
+          const pattern = /^[0-9]+([.]{1}[0-9]{1,2})?$/;
+          return pattern.test(value) || "请输入正确金额格式";
+        }
+      }
+    };
   },
   computed: {
     openMyAccount: {
