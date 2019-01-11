b8b4c532d1874476bc6f4f44fb2208af5cf1a464
Vleboy <vleboy@gmail.com>
2019-01-11 14:44:55 +0800

    fix pre user

--- src/middleware/pre-user.js
+++ src/middleware/pre-user.js
@@ -86,9 +86,7 @@ router.post('/user/insert', async (ctx, next) => {
         if (r[0].levelIndex.indexOf(a[0].id) === -1) {
             throw { err: true, res: '该安置编码不合理' }//该安置编码不合理
         }
-        inparam.levelIndex = r[0].levelIndex
-        inparam.levelIndex.push(inparam.id)
-        inparam.createdAt = Date.now()
+        
     } else {
         if (inparam.parentId === 'root') {
             inparam.levelIndex = [inparam.id]//系统第一人
@@ -101,6 +99,9 @@ router.post('/user/insert', async (ctx, next) => {
     if (r.length > 0) {
         throw { err: true, res: '网络错误，请重试' }
     } else {
+        inparam.levelIndex = r[0].levelIndex
+        inparam.levelIndex.push(inparam.id)
+        inparam.createdAt = Date.now()
         inparam.balance = 0; inparam.iswechatpay = false
         ctx.body = { err: false, res: inparam.id }  // 返回检查结果和生成的openid
         return next()
