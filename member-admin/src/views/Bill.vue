cbaa520dfffeab4dc89057297a98d120ea8f19df
cheneyweb <457299596@qq.com>
2019-01-12 21:17:15 +0800

    平台账单列表

--- member-admin/src/views/Bill.vue
+++ member-admin/src/views/Bill.vue
@@ -1,23 +1,32 @@
 <template>
   <v-container fluid fill-height justify-center>
     <v-flex xs12>
-      <v-data-table :headers="headers" :items="desserts" hide-actions no-data-text="暂无数据">
+      <v-data-table :headers="headers" :items="bills" hide-actions no-data-text="暂无数据">
         <template slot="items" slot-scope="props">
-          <td>{{ props.item.createdAt }}</td>
+          <td>{{ props.item.createdAt | formatDate}}</td>
           <td>{{ props.item.project }}</td>
           <td>{{ props.item.userId }}</td>
           <td>{{ props.item.remark }}</td>
-          <td>{{ props.item.type }}</td>
+          <td>{{ props.item.type =='IN' ? '收' : '支' }}</td>
           <td>{{ props.item.amount }}</td>
           <td>{{ props.item.balance }}</td>
         </template>
       </v-data-table>
     </v-flex>
+    <!--错误提示-->
+    <v-snackbar v-model="snackMsg.isShow" top auto-height :color="snackMsg.color">
+      {{snackMsg.msg}}
+      <v-btn flat @click="snackMsg.isShow = false">关闭</v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
 <script>
+import dayjs from "dayjs";
 export default {
+  created: function() {
+    this.billQuery();
+  },
   data() {
     return {
       headers: [
@@ -29,19 +38,29 @@ export default {
         { text: "金额", value: "amount", sortable: false },
         { text: "余额", value: "balance", sortable: false }
       ],
-      desserts: [
-        {
-          createdAt: "2019-01-01 12:30:00",
-          project: "会员注册",
-          userId: "MY100001",
-          userId: "MY10001",
-          remark: "",
-          type: "收",
-          amount: 1000,
-          balance: 1000.0
-        }
-      ]
+      bills: [],
+      query: {},
+      snackMsg: {
+        isShow: false,
+        color: "success",
+        msg: ""
+      }
     };
+  },
+  methods: {
+    async billQuery() {
+      this.$store.commit("openLoading", true);
+      let res = await this.$store.dispatch("billQuery", this.query);
+      if (!res.err) {
+        this.bills = res.res;
+      }
+      this.$store.commit("openLoading", false);
+    }
+  },
+  filters: {
+    formatDate(timestamp) {
+      return dayjs(timestamp).format("YY/MM/DD HH:mm:ss");
+    }
   }
 };
 </script>
