d37e6d2485551c39fa2749391c8ef2e958fc0399
Vleboy <vleboy@gmail.com>
2019-01-13 23:20:33 +0800

    删除test

--- src/server/test.js
+++
@@ -1,6 +0,0 @@
-const _ = require('lodash')
-const achievements =[{userid:'MX1',price:300},{userid:'MX2',price:300},{userid:'MX2',price:300},{userid:'MX1',price:300},{userid:'MX1',price:300},
-{userid:'MX1',price:300},{userid:'MX3',price:300},{userid:'MX3',price:300},{userid:'MX3',price:300},{userid:'MX1',price:300}
-]
-let r =_.filter(achievements, _.iteratee(['userid', 'MX1']));
-console.log(r)
